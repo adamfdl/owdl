c35a484d7f4275540baeb8b4b84722d2d7bd032b
Adam Fadhil <adamfdls@gmail.com>
2018-02-20 21:09:50 +0700

    [OWDL-008] Added OWDL_DEV bot token

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func init() {
 }
 
 func main() {
-	dg, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	dg, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN_DEV"))
 	if err != nil {
 		log.Error().
 			Msgf("Failed creating discord session. Error: %s", err.Error())
